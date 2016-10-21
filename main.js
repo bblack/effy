5255a849e94583e527a2b275ac603b0af6aa40b5
Brian Black <black@gatech.edu>
2016-10-21 10:33:11 -0400

    add start script

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
 var app = require('./app');
-app.listen(5050);
-console.log('listening...');
+app.listen(3000 || process.env.PORT, function(){
+  console.log('listening on %s', JSON.stringify(this.address()));
+});
